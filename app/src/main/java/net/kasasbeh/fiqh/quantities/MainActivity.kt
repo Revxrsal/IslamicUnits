945c3b7bfdfab49c8cb686a1179851790944d09c
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-11-26 22:06:28 +0300

    init commit

---
+++ app/src/main/java/net/kasasbeh/fiqh/quantities/MainActivity.kt
@@ -0,0 +1,27 @@
+package net.kasasbeh.fiqh.quantities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.kasasbeh.fiqh.quantities.ui.theme.FiqhQuantitiesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FiqhQuantitiesTheme {
+                Surface {  }
+            }
+        }
+    }
+}
